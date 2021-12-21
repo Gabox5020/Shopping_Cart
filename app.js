20b01c57a3dffdc4941504fe09e4995615c171fd
Gabriel <gaboxroll.2@gmail.com>
2021-12-21 14:25:26 -0300

    Segundo Ejemplo

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
 })
@@ -6,8 +10,20 @@ const fetchData = async () => {
     try {
         const res = await fetch('api.json')
         const data = await res.json()
-        console.log(data)
+        // console.log(data)
+        pintarCards(data)
     } catch (error) {
         console.log(error)
     }
+}
+
+const pintarCards = data => {
+    data.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.title
+        templateCard.querySelector('p').textContent = producto.precio
+
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
 }
